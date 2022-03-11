540a01831bbd70a0bb7b8297aa74ec60aea4b32d
SanusiWasiu <was.sanusi@gmail.com>
2022-03-11 03:10:25 -0800

    write C program to print sizeof of various type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i2));
-	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d);
-	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f);
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
