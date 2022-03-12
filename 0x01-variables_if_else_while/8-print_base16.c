247c8d2563afbd76ee0e9e6f467f1afa7c2020b1
SanusiWasiu <was.sanusi@gmail.com>
2022-03-12 14:46:31 -0800

    add c file to print numbers under base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - it happens here
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	int i = 0;
+
+	while (i < 16)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else
+		{
+			putchar(ch);
+			ch++;
+		};
+		i++;
+	};
+
+	return (0);
+}
