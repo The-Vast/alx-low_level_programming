c578ca96bc67e49ddb75b0b9af687cb8af4621b8
SanusiWasiu <was.sanusi@gmail.com>
2022-03-12 13:47:21 -0800

    add c file that prints numbers under 10 without using Char funcs

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - where all the codes are cooked
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+	putchar('\n');
+
+	return (0);
+}
