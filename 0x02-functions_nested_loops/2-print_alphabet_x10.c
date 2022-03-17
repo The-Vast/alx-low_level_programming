e9ad8475dd12caa39a589575e57eab7dea24397d
SanusiWasiu <was.sanusi@gmail.com>
2022-03-16 21:41:56 -0700

    add script to print all alphabetsX10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include
+
+/**
+ * print_alphabetx10 - it prints all alpha 10 times
+ * Return - nada
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int num = 0;
+
+	while (num < 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+		num++;
+	}
+}
