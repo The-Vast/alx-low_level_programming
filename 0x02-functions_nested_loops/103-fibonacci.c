9e13f8b02c369066928cda89534d261365a9444b
SanusiWasiu <was.sanusi@gmail.com>
2022-03-17 04:24:41 -0700

    add program to print fibonacci sum of even

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - power house
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	float total;
+	unsigned long num1 = 0, num2 = 1, sum;
+
+	for (count = 0; count <= 4000000; count++)
+	{
+		sum = num1 + num2;
+		if ((sum % 2) == 0)
+			total += sum;
+
+		num1 = num2;
+		num2 = sum;
+	}
+	printf("%.0f\n", total);
+
+	return (0);
+}
