753f2e004f6a2b76bdb2312c6ed482a031ae2079
SanusiWasiu <was.sanusi@gmail.com>
2022-03-17 04:02:19 -0700

    add program that print 1st 50 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - action house
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int num, num1 = 0, num2 = 1, sum;
+
+	for (num = 0; num < 50; num++)
+	{
+		sum = num1 + num2;
+		printf("%d", sum);
+
+		if (num == 49)
+			printf('\n');
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
