48652cc8659a3c42fa2c0ff2be5bfbd636cdbb11
SanusiWasiu <was.sanusi@gmail.com>
2022-03-16 22:43:27 -0700

    add program to print signs based on number state

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints sign
+ * n: integer to be checked
+ * Return: n > 0; 1 is returned, 
+ * 	   n = 0 ; 0 is returned, 
+ * 	   n < 0; -1 is returned
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
