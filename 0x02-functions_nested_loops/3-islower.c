2782c7020cb6317f8465d63531f5a0fd96cdda1c
SanusiWasiu <was.sanusi@gmail.com>
2022-03-16 21:52:42 -0700

    add script to check for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+/**
+ * _islower - Checks for lowercase state
+ * Return: 1 if lower or 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else:
+		return (0);
+}
