749ea7388c70346e37c3028f6edec7b392aaff1e
SanusiWasiu <was.sanusi@gmail.com>
2022-03-18 17:27:20 -0700

    add function to print diagonal and fix bug

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line on the terminal.
- * @n: number of times the character \ should be printed
+ ** print_diagonal - draws a diagonal line on the terminal
+ ** @n: number of times the character \ should be print
  */
 void print_diagonal(int n)
 {
@@ -16,11 +16,14 @@ void print_diagonal(int n)
 
 		for (i = 0; i < n; i++)
 		{
-			if (j == i)
-				_putchar('\\');
-			else if (j < i)
-				_putchar(' ');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
