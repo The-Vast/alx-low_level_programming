a9ed24d38ad14bf12cc5384e024f3fa6d34c7dab
SanusiWasiu <was.sanusi@gmail.com>
2022-03-23 16:03:43 -0700

    add function to reverse content of an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: array address
+ * @n: number array elemnts
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		temp = *(a + j);
+		*(a + j) = *(a + (j - 1));
+		*(a + (j - 1)) = temp;
+	}
+}
