ab40950bcd351a75f2cf594fb215fece6344326f
SanusiWasiu <was.sanusi@gmail.com>
2022-03-23 15:30:47 -0700

    add function to concat with condition

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,12 +7,16 @@
  * @n: max byte from src
  * Return: pointer to the resulting string dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
 
 	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
 	{
 		*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
@@ -20,5 +24,6 @@ char *_strncat(char *dest, char *src, int n)
 		count++;
 		count2++;
 	}
+
 	return (dest);
 }
