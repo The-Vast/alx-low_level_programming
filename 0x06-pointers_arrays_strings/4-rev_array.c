62f5c1b2e87279187880170577fcfa5a5609d228
SanusiWasiu <was.sanusi@gmail.com>
2022-03-24 02:14:35 -0700

    fix output bug

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,8 +12,11 @@ void reverse_array(int *a, int n)
 
 	for (i = 0; i < n - 1; i++)
 	{
-		temp = *(a + j);
-		*(a + j) = *(a + (j - 1));
-		*(a + (j - 1)) = temp;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
 }
