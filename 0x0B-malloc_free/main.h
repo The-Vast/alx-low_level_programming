62669892ab796c8cb88b6f3f603bfe803df935e0
SanusiWasiu <was.sanusi@gmail.com>
2022-04-06 17:06:16 -0700

    fix newline bug

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: holberton.h
- * Auth: Wasiu Sanusi
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-malloc_free directory.
- */
-
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
