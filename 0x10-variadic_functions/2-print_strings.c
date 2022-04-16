b805122080f9bb0e94ccbf8fc5d0468f9cbbe16e
SanusiWasiu <was.sanusi@gmail.com>
2022-04-15 17:37:23 -0700

    remove newline in all files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,30 +9,30 @@
  * @...: A variable number of strings to be printed.
  *
  * Description: If separator is NULL, it is not printed.
- *              If one of the strings if NULL, (nil) is printed instead.
+ * If one of the strings if NULL, (nil) is printed instead.
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list strings;
-    unsigned int index;
-    char *str;
+	va_list strings;
+	unsigned int index;
+	char *str;
 
-    va_start(strings, n);
+	va_start(strings, n);
 
-    for (index = 0; index < n; index++)
-    {
-        str = va_arg(strings, char *);
-
-        if (str == NULL)
-            printf('nil');
-        else
-            printf("%s", str);
-
-        if (index != (n - 1) && separator != NULL)
-            printf("%s", separator);
-    }
-    printf("\n");
+	for (index = 0; index < n; index++)
+	{
+		str = va_arg(strings, char *);
+	
+		if (str == NULL)
+			printf('nil');
+		else
+			printf("%s", str);
+		
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
 
 	va_end(strings);
 }
