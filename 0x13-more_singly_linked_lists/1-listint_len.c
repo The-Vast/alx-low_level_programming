b2a11ae28af4193289e35cbb1edc7389d32b52fd
SanusiWasiu <was.sanusi@gmail.com>
2022-04-27 01:33:47 +0100

    add function to print length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - Returns the number of elements
+ *               in a linked listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements in the listint_t list.
+ */
+size_t listint_len(const listint_t *h)
+{
+    int node_len = 0;
+
+    while (h)
+    {
+        node_len++;
+        h = h->next;
+    }
+}
